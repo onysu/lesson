bbe0c34f1e4c75228e6ce281958407216999ead2
onysu <onydarrell@gmail.com>
2021-07-31 16:16:09 +0700

    M

--- dist/js/all-style.js
+++ dist/js/all-style.js
@@ -1,11 +1,10 @@
+// script tombol kembali keatas
 // Dapatkan tombolnya
 var mybutton = document.getElementById("myBtn");
-
 // Saat pengguna menggulir ke bawah 20px dari atas dokumen, perlihatkan tombol
-window.onscroll = function() {
+window.onscroll = function () {
     scrollFunction()
 };
-
 function scrollFunction() {
     if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
         mybutton.style.display = "block";
@@ -13,9 +12,57 @@ function scrollFunction() {
         mybutton.style.display = "none";
     }
 }
-
 // Ketika pengguna mengklik tombol, gulir ke atas dokumen
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+// script video pengenalan
+var starttime = 2;
+$(function () {
+    $("#playlist li").on("click", function () {
+        $("#videoarea-cd").attr({
+            src: $(this).attr("movieurl"),
+            poster: "",
+            autoplay: "autoplay"
+        });
+        // starttime = $(this).attr("startt");
+    });
+    // $("#videoarea-cd").attr({
+    //  "src": $("#playlist li").eq(0).attr("movieurl"),
+    //  "poster": $("#playlist li").eq(0).attr("moviesposter"),
+    // })
+});
+
+// document.getElementById("videoarea-cd").addEventListener(
+//  "loadedmetadata",
+//   function () {
+//     this.currentTime = starttime;
+//   },
+//   false
+// );
+
+
+// cari dengan nama video
+$(document).ready(function () {
+    $("#myInput").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#myList article").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+
+
+// autoplay dengan mouse
+// const clip = document.querySelectorAll('.clip');
+// for (let i = 0; i < clip.length; i++) {
+//     clip[i].addEventListener('mouseenter', function (e) {
+//         clip[i].play()
+//     });
+//     clip[i].addEventListener('mouseout', function (e) {
+//         clip[i].pause()
+//     });
+// }

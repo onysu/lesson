b781ea66d0208172bf456cc4eb33b58e17654d04
ony <onydarrell@gmail.com>
2021-08-29 07:16:43 +0700

    M

--- dist/js/waktu.js
+++ dist/js/waktu.js
@@ -1,28 +1,10 @@
-// Tetapkan tanggal yang kita hitung mundur
-var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
-
-// Perbarui hitungan mundur setiap 1 detik
-var x = setInterval(function() {
-
-    // Dapatkan tanggal dan waktu hari ini
-    var now = new Date().getTime();
-
-    // Temukan jarak antara sekarang dan tanggal hitung mundur
-    var distance = countDownDate - now;
-
-    // Perhitungan waktu untuk hari, jam, menit dan detik
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Keluarkan hasilnya dalam elemen dengan id="demo"
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
-        minutes + "m " + seconds + "s ";
-
-    // Jika hitungan mundur selesai, tulis beberapa teks
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-}, 1000);
+const d = new Date();
+const days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+document.getElementById("today").innerHTML = days[d.getDay()];
+const months = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+document.getElementById("tgl").innerHTML = d.getDate();
+document.getElementById("bulan").innerHTML = months[d.getMonth()];
+document.getElementById("tahun").innerHTML = d.getFullYear();
+document.getElementById("jam").innerHTML = d.getHours();
+document.getElementById("menit").innerHTML = d.getMinutes();
+document.getElementById("detik").innerHTML = d.getSeconds();

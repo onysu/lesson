7b0875366c4f27560d51895190b1ec2c18d1e691
ony <onydarrell@gmail.com>
2021-09-21 02:36:12 +0700

    M

--- sticky/main.js
+++
@@ -1,9 +0,0 @@
-stickybits('header', {
-    useStickyClasses: true
-});
-
-const headerHeight = document.querySelector('header').offsetHeight;
-
-stickybits('.signup', {
-    stickyBitStickyOffset: headerHeight
-})
